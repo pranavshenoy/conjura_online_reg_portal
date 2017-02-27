0c83459fc1f1cfe39cb2b13156c1c83a26e2cab6
PRANAV A SHENOY <pranav@PRANAV>
2017-02-27 13:56:17 +0530

    formatting everything and include function used

--- final.php
+++ final.php
@@ -1,9 +1,9 @@
 <?php  
 	require_once("db_connect.php");
-	echo $_SESSION['new_user'];
+
 	function insert_participant($name,$email,$phone,$accommodation,$gender,$college)
 	{
-		
+		$query="INSERT INTO participants (name,college,email,phone,gender,accommodation) VALUES   ('$fullname','$college','$email','$phone','$gender','$accommodation')"
 	
 	}
 
