bd3f493d9dacf2e8442d9032a3da7ed9853b4ea2
PRANAV A SHENOY <pranav@PRANAV>
2017-02-27 23:43:18 +0530

    included an if statement in final.php

--- final.php
+++ final.php
@@ -46,78 +46,81 @@
 		}	
 		return -1;
 	}
+	$temp_flag=1;
 	
-	if($_SESSION['new_user']==1)
+	if($temp_flag==1)
 	{
+		$temp_flag=0;
+		if($_SESSION['new_user']==1)
+		{
+			$_SESSION['new_user']=0;
+			$result = $con->query($_SESSION['query']) or die(mysqli_error($con)); 
+			$_SESSION['head_id']=get_part_id($_SESSION['email_head'],$_SESSION['phone_head'],$con);
+		}
+		//store in transactions database
+		$trans_id=cleanup($_POST['trans_id'],$con);
+		$acnt_no=cleanup($_POST['ac_no'],$con);
+	
+		
+		$verify=insert_transaction($_SESSION['head_id'],$_SESSION['total_amt'],$trans_id,$acnt_no,$con);
+		if($verify!=1)
+		{
+			$_SESSION['ERROR']="Registraion Incomplete";
+			header("location:index.php");
+		}	
+	
+		//store in event_participation
+		if(isset($_POST['trans_id']) && isset($_POST['ac_no']))
+			$paid='y';
+		else
+			$paid='n';
+			
+		for($i=1;$i<=$_SESSION['e_count'];$i++)	
+		{	
+			$verify=insert_event_part ($_SESSION['head_id'],$_SESSION['event_id'][$i],$paid,$trans_id,$con);
+			if($verify!=1)
+			{
+				$_SESSION['ERROR']="Registraion Incomplete";
+				header("location:index.php");
+			}
+		}
+	
+		//add member participants
 
-		$result = $con->query($_SESSION['query']) or die(mysqli_error($con)); 
-		$_SESSION['head_id']=get_part_id($_SESSION['email_head'],$_SESSION['phone_head'],$con);
-	}
-	
-	
-
-	//store in transactions database
-	$trans_id=cleanup($_POST['trans_id'],$con);
-	$acnt_no=cleanup($_POST['ac_no'],$con);
-	
-	
-	$verify=insert_transaction($_SESSION['head_id'],$_SESSION['total_amt'],$trans_id,$acnt_no,$con);
-	if($verify!=1)
-	{
-		$_SESSION['ERROR']="Registraion Incomplete";
+		for($i=1;$i<=$_SESSION['team_count'];$i++)
+		{
+			$fullname=cleanup($_POST['fullName'.$i],$con);
+			$college=cleanup($_POST['college'.$i],$con);
+			$email=cleanup($_POST['email'.$i],$con);
+			$phone=cleanup($_POST['phone'.$i],$con);
+			$gender=$_POST['gender'.$i];
+		
+			if(isset($_POST['accommodation'.$i]))
+				$accommodation='y';
+			else	
+				$accommodation='n';
+			
+			$verify=insert_participant($fullname,$email,$phone,$accommodation,$gender,$college,$con);
+			if($verify==1)
+			{
+				$member_id=get_part_id($email,$phone,$con);
+			}
+			else // verify is the member id
+			{
+				$member_id=$verify;
+			}
+		
+			// adding to team table
+		
+			$verify=insert_team ($_SESSION['team_event_id'][$i],$_SESSION['head_id'],$member_id,$con);
+			if($verify!=1)
+			{
+				$_SESSION['ERROR']="Registraion Incomplete";
+				header("location:index.php");
+			}
+		}
+		$_SESSION['MESSAGE']="Registraion Successfull. Your Conjura ID is CT".$_SESSION['head_id'];
 		header("location:index.php");
+		require_once("initialise.php");	
 	}	
-	
-	//store in event_participation
-	if(isset($_POST['trans_id']) && isset($_POST['ac_no']))
-		$paid='y';
-	else
-		$paid='n';
-			
-	for($i=1;$i<=$_SESSION['e_count'];$i++)	
-	{	
-		$verify=insert_event_part ($_SESSION['head_id'],$_SESSION['event_id'][$i],$paid,$trans_id,$con);
-		if($verify!=1)
-		{
-			$_SESSION['ERROR']="Registraion Incomplete";
-			header("location:index.php");
-		}
-	}
-	
-	//add member participants
-
-	for($i=1;$i<=$_SESSION['team_count'];$i++)
-	{
-		$fullname=cleanup($_POST['fullName'.$i],$con);
-		$college=cleanup($_POST['college'.$i],$con);
-		$email=cleanup($_POST['email'.$i],$con);
-		$phone=cleanup($_POST['phone'.$i],$con);
-		$gender=$_POST['gender'.$i];
-		
-		if(isset($_POST['accommodation'.$i]))
-			$accommodation='y';
-		else	
-			$accommodation='n';
-			
-		$verify=insert_participant($fullname,$email,$phone,$accommodation,$gender,$college,$con);
-		if($verify==1)
-		{
-			$member_id=get_part_id($email,$phone,$con);
-		}
-		else // verify is the member id
-		{
-			$member_id=$verify;
-		}
-		
-		// adding to team table
-		
-		$verify=insert_team ($_SESSION['team_event_id'][$i],$_SESSION['head_id'],$member_id,$con);
-		if($verify!=1)
-		{
-			$_SESSION['ERROR']="Registraion Incomplete";
-			header("location:index.php");
-		}
-	}
-	$_SESSION['MESSAGE']="Registraion Successfull. Your Conjura ID is CT".$_SESSION['head_id'];
-	header("location:index.php");	
 ?>
