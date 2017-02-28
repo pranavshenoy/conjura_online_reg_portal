840106cb7833aad9ced6bb5ea01ed4d537c28b6a
Pranav Shenoy <pranavshenoy06@gmail.com>
2017-03-01 00:26:59 +0530

    bug fix- if a member was already registered and tries to enter different email or phone no. error will be displayed

--- final.php
+++ final.php
@@ -7,13 +7,15 @@
 		{
 			$query="INSERT INTO participants (name,college,email,phone,gender,accommodation) VALUES   ('$name','$college','$email','$phone','$gender','$accommodation')";
 			$result = $con->query($query);		
-			if($result==TRUE)	
-				return 1;
+			
+			if($result)	
+				return get_part_id($email,$phone,$con);
 			else 
 			 	return -1;	
 		}
 		return $temp;         //id
 	}
+	
 	function insert_event_part ($part_id,$event_id,$paid,$trans_id,$con)
 	{
 		$query="INSERT INTO event_participants (part_id,event_id,paid,trans_id) VALUES   ($part_id,$event_id,'$paid','$trans_id')";
@@ -55,6 +57,7 @@
 		}	
 		return -1;
 	}
+	
 	$temp_flag=1;
 	
 	if($temp_flag==1)
@@ -75,6 +78,30 @@
 			$_SESSION['head_id']=get_part_id($_SESSION['email_head'],$_SESSION['phone_head'],$con);
 		}
 		
+			// insert members
+		for($i=1;$i<=$_SESSION['team_count'];$i++)
+		{
+			$fullname=cleanup($_POST['fullName'.$i],$con);
+			$college=cleanup($_POST['college'.$i],$con);
+			$email=cleanup($_POST['email'.$i],$con);
+			$phone=cleanup($_POST['phone'.$i],$con);
+			$gender=$_POST['gender'.$i];
+			if(isset($_POST['accommodation'.$i]))
+				$accommodation='y';
+			else	
+				$accommodation='n';	
+
+			$verify=insert_participant($fullname,$email,$phone,$accommodation,$gender,$college,$con);
+		 
+		  if($verify==-1)  // verify is the member id
+			{
+				unset($_SESSION['MESSAGE']);
+				$_SESSION['ERROR']="Registration Incomplete.".strtoupper($fullname)."  has already registered using other credentials";
+				header("location:index.php");
+				return;
+			}
+		}
+		
 		//store in transactions database
 		$trans_id=cleanup($_POST['trans_id'],$con);
 		$acnt_no=cleanup($_POST['ac_no'],$con);
@@ -82,7 +109,7 @@
 		if(isset($_POST['trans_id']) && isset($_POST['ac_no']))
 		{
 			$verify=insert_transaction($_SESSION['head_id'],$_SESSION['total_amt'],$trans_id,$acnt_no,$con);
-			if($verify!=1)
+			if($verify==-1)
 			{
 				unset($_SESSION['MESSAGE']);
 				$_SESSION['ERROR']="Registraion Incomplete";
@@ -96,11 +123,10 @@
 			$paid='y';
 		else
 			$paid='n';
-			
 		for($i=1;$i<=$_SESSION['e_count'];$i++)	
 		{	
 			$verify=insert_event_part ($_SESSION['head_id'],$_SESSION['event_id'][$i],$paid,$trans_id,$con);
-			if($verify!=1)
+			if($verify==-1)
 			{
 				unset($_SESSION['MESSAGE']);
 				$_SESSION['ERROR']="Registraion Incomplete";
@@ -108,36 +134,25 @@
 				return;
 			}
 		}
-	
-		//add member participants
 
+						// adding to team table
 		for($i=1;$i<=$_SESSION['team_count'];$i++)
 		{
-			$fullname=cleanup($_POST['fullName'.$i],$con);
-			$college=cleanup($_POST['college'.$i],$con);
 			$email=cleanup($_POST['email'.$i],$con);
 			$phone=cleanup($_POST['phone'.$i],$con);
-			$gender=$_POST['gender'.$i];
-		
-			if(isset($_POST['accommodation'.$i]))
-				$accommodation='y';
-			else	
-				$accommodation='n';	
-			$verify=insert_participant($fullname,$email,$phone,$accommodation,$gender,$college,$con);
-			if($verify==1)
-				$member_id=get_part_id($email,$phone,$con);
-			else // verify is the member id
-				$member_id=$verify;
-		
-			// adding to team table
-			$verify=insert_team ($_SESSION['team_event_id'][$i],$_SESSION['head_id'],$member_id,$con);
-			if($verify!=1)
+			$member_id=get_part_id($email,$phone,$con);
+			
+			if($member_id!=-1)
 			{
-				unset($_SESSION['MESSAGE']);
-				$_SESSION['ERROR']="Registraion Incomplete";
-				header("location:index.php");
-				return;
-			}
+				$verify=insert_team ($_SESSION['team_event_id'][$i],$_SESSION['head_id'],$member_id,$con);
+				if($verify==-1)
+				{
+					unset($_SESSION['MESSAGE']);
+					$_SESSION['ERROR']="Registraion Incomplete";
+					header("location:index.php");
+					return;
+				}
+			}					
 		}
 		$_SESSION['MESSAGE']="Registraion Successfull. Your Conjura ID is CT".$_SESSION['head_id'];
 		header("location:index.php");
