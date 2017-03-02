0c03fae89f1b7d99946f69e19f5378ce641dcd99
Pranav Shenoy <pranavshenoy06@gmail.com>
2017-03-02 19:45:50 +0530

    added serial number in cross_check.php

--- cross_check.php
+++ cross_check.php
@@ -12,17 +12,18 @@
 	require_once("db_connect.php");
 	$query="SELECT id,name,college,email,phone,trans_id,amt from participants as p,transactions as t where p.id = t.part_id ";
 	$result = $con->query($query);
+	$count=0;
 	if($result->num_rows>0)
 	{
 		echo "<table class='table table-striped'>";
 		echo "<tr>";
-		echo "<th>CONJURA ID</th><th>NAME</th><th>COLLEGE</th><th>EMAIL</th><th>PHONE</th><th>TRANS_ID</th><th>AMT</th>";
+		echo "<th>S.NO</th><th>CONJURA ID</th><th>NAME</th><th>COLLEGE</th><th>EMAIL</th><th>PHONE</th><th>TRANS_ID</th><th>AMT</th>";
 		echo "</tr>";
 		
 		while($row=$result->fetch_assoc())
 		{
 			echo "<tr>";
-			echo "<td>".$row['id']."</td><td>".$row['name']."</td><td>".$row['college']."</td><td>".$row['email']."</td><td>".$row['phone']."</td><td>".$row['trans_id']."</td><td>".$row['amt']."</td>";
+			echo "<td>".++$count."</td><td>".$row['id']."</td><td>".$row['name']."</td><td>".$row['college']."</td><td>".$row['email']."</td><td>".$row['phone']."</td><td>".$row['trans_id']."</td><td>".$row['amt']."</td>";
 			echo "</tr>";
 		}		
 		echo "</table>";
